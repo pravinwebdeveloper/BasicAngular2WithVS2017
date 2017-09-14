e51d9d192d5f42724aa46d76a8465dbf6f90e527
Pravin Kumar <pravin.webdeveloper@live.com>
2017-09-14 14:20:39 +0530

    Basic QuickStartFiles
    Angular2 QuickStart Files

---
+++ Angular2App/Scripts/app/app.component.ts
@@ -0,0 +1,8 @@
+﻿import {Component} from "@angular/core";
+
+@Component({
+    selector: "opengamelist",
+    template: `<h1>Welcome</h1><div>...the best is yet to come!</div>`
+})
+
+export class AppComponent { }
